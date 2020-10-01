ba7cd199323da841e0f8edd886ed8d92dd84f51b
User <uzairbangee@gmail.com>
2020-10-01 23:47:19 +0500

    change in the elements

---
+++ dist/src/components/TaskScreen/TaskScreen.tsx
@@ -0,0 +1,25 @@
+import React, { ReactElement } from 'react'
+import "./TaskScreen.css";
+import TaskLists from '../TaskLists/TaskLists';
+// import {useSelector} from "react-redux";
+// import {RootState} from "../../store";
+
+interface Props {
+    
+}
+
+function TaskScreen({}: Props): ReactElement {
+    const tasks = useSelector((state: RootState) => state.tasks);
+    return (
+        <div className="page lists_show">
+            <nav>
+                <h1 className="title-page">
+                    <span className="title-wrapper">Taskbox</span>
+                </h1>
+            </nav>
+            <TaskLists tasks={tasks} loading={false} />
+        </div>
+    )
+}
+
+export default TaskScreen;
